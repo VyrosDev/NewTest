23543e717222ba2fb331e66c017b290ab1cc9a01
Vyros Hub <176984240+CkVyros@users.noreply.github.com>
2024-12-08 18:35:44 -0300

    teste3.lua

--- teste3.lua
+++ teste3.lua
@@ -21,7 +21,7 @@ ImageButton.BorderColor3 = Color3.fromRGB(255, 0, 0)
 ImageButton.Draggable = true
 ImageButton.Position = UDim2.new(0.102097899, 0, 0.0742971897, 0)
 ImageButton.Size = UDim2.new(0, 59, 0, 49)
-ImageButton.Image = "http://www.roblox.com/asset/?id=82291816564081"
+ImageButton.Image = "http://www.roblox.com/asset/?id=12870475640"
 
 UICorner.Name = "MainCorner"
 UICorner.CornerRadius = UDim.new(0, 9)
